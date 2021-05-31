814eb56d7c8f00123a3b10e18a8884ea64100640
zvezdochiot <mykaralw@yandex.ru>
2021-05-31 20:52:52 +0300

    0.2: support jp2

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/ImageProcessing-ElectronicPublications/python-pdf-jpeg-extract)
+![GitHub Release Date](https://img.shields.io/github/release-date/ImageProcessing-ElectronicPublications/python-pdf-jpeg-extract)
+![GitHub repo size](https://img.shields.io/github/repo-size/ImageProcessing-ElectronicPublications/python-pdf-jpeg-extract)
+![GitHub all releases](https://img.shields.io/github/downloads/ImageProcessing-ElectronicPublications/python-pdf-jpeg-extract/total)
+![GitHub](https://img.shields.io/github/license/ImageProcessing-ElectronicPublications/python-pdf-jpeg-extract)
+
 # Extract jpeg images from pdf
 
-Simple script for extract jpeg images from pdf.
+Simple script for extract jpeg and jp2 images from pdf.
 
 Use python2.
 
--- pdf-jpeg-extract-dirty.py
+++ pdf-jpeg-extract-dirty.py
@@ -7,6 +7,7 @@ with open(sys.argv[1],"rb") as file:
     pdf = file.read()
 
 startmark = b"\xff\xd8"
+startmark2 = b"\x00\x00\x00\x0C"
 startfix = 0
 endmark = b"\xff\xd9"
 endfix = 2
@@ -18,22 +19,34 @@ while True:
     if istream < 0:
         break
     istart = pdf.find(startmark, istream, istream + 20)
-    if istart < 0:
+    istart2 = pdf.find(startmark2, istream, istream + 20)
+    if (istart < 0) and (istart2 < 0):
         i = istream + 20
         continue
-    iend = pdf.find(b"endstream", istart)
+    if istart2 < 0:
+        iend = pdf.find(b"endstream", istart)
+    if istart < 0:
+        iend = pdf.find(b"endstream", istart2)
     if iend < 0:
         raise Exception("Didn't find end of stream!")
     iend = pdf.find(endmark, iend - 20)
     if iend < 0:
         raise Exception("Didn't find end of JPG!")
 
-    istart += startfix
-    iend += endfix
-    print("JPG %d from %d to %d" % (njpg, istart, iend))
-    jpg = pdf[istart:iend]
-    with open("jpg%04d.jpg" % njpg, "wb") as jpgfile:
-        jpgfile.write(jpg)
+    if istart2 < 0:
+        istart += startfix
+        iend += endfix
+        print("JPG %d from %d to %d" % (njpg, istart, iend))
+        jpg = pdf[istart:iend]
+        with open("jpg%04d.jpg" % njpg, "wb") as jpgfile:
+            jpgfile.write(jpg)
+    if istart < 0:
+        istart2 += startfix
+        iend += endfix
+        print("JP2 %d from %d to %d" % (njpg, istart, iend))
+        jp2 = pdf[istart2:iend]
+        with open("jpg%04d.jp2" % njpg, "wb") as jpxfile:
+            jpxfile.write(jp2)
 
     njpg += 1
     i = iend
